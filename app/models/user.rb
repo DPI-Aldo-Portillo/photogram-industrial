362b1a601b5ebae16aa887409df2af58a06d8081
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-12 20:21:52 +0000

    Write indirect and scoped associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,21 @@ class User < ApplicationRecord
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes foreign_key: :fan_id
+
+  # Indirect Associations
+  has_many :liked_photos, through: :likes, source: :photos
+
+  ## Uses scoped association to get the instances of FollowRequests and get Users
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
+  has_many :feed, through: :leaders, source: :own_photos
+  has_many :discover, through: :leaders, source: :liked_photos
+  #Scoped
+
+  ## Means following
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  ## Means followers
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+
 end
