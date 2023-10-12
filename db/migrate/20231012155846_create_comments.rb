7975e3f4f1edc714b1c69149b8339af68fecc470
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-12 16:08:53 +0000

    Create and migrate comment db

--- db/migrate/20231012155846_create_comments.rb
+++ db/migrate/20231012155846_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, null: false, foreign_key: true
-      t.references :photo, null: false, foreign_key: true
-      t.text :body
+      t.references :author, null: false, foreign_key: {to_table: :users}, index: false ## We use to_table: :users since author isn't the name of a table. We need to connect it to users table
+      t.references :photo, null: false, foreign_key: true ##Unlike :author, :photo is a table so we just set foreign_key: tue
+      t.text :body, null: false
 
       t.timestamps
     end
